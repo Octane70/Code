634597abac6b9de00b5f9af5d5bfcd81602bf857
Rob Czepil <robczepil@gmail.com>
2015-03-23 20:45:06 -0700

    Update servo_test_b.py

--- Maestro/servo_test_b.py
+++ Maestro/servo_test_b.py
@@ -14,7 +14,7 @@ servoMax = 9500
 
 # Add servo increments
 servoPan = 6000
-servostepPan = 100
+servostepPan = 500
 
 def getch():
 	fd = sys.stdin.fileno()
@@ -41,7 +41,7 @@ def servo1_right():
   if servoPan < servoMin:
     servoPan = servoMin
   servo.setAccel(0,4)      #set servo 0 acceleration to 4
-  servo.setTarget(0,2500)  #set servo to move to center position
+  servo.setTarget(0,servoPan)  #set servo to move to center position
   servo.close
 
 def servo_return():
