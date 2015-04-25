fcd40a2c969dbaea511e295ceca37fb197e9da4f
robczepil@gmail.com <robczepil@gmail.com>
2015-04-25 00:11:46 -0700

    Replace terminal with text widget

--- GUI/Garden_GUI_v0.1.py
+++ GUI/Garden_GUI_v0.1.py
@@ -3,6 +3,7 @@ import sys
 from Tkinter import *
 import time
 import os
+import subprocess as sub
 
 display = Tk()
 time1 = ''
@@ -29,11 +30,11 @@ L13 = Label(display, text ="Evening Start").grid(row=17, column=0, sticky=W)
 L14 = Label(display, text ="Evening Stop").grid(row=18, column=0, sticky=W)
 L15 = Label(display, text ="Moisture Min").grid(row=19, column=0, sticky=W)
 L16 = Label(display, text ="Moisture Max").grid(row=20, column=0, sticky=W)
-L17 = Label(display, text ="Camera 1").grid(row=0, column=20, sticky=W)
-L18 = Label(display, text ="Camera 2").grid(row=18, column=20, sticky=W)
-#Text
-#def retrieve_input():
+L17 = Label(display, text ="Camera 1:").grid(row=0, column=23, sticky=W)
+L18 = Label(display, text ="Camera 2:").grid(row=18, column=23, sticky=W)
+L19 = Label(display, text ="Shell Output:").grid(row=22, column=0, sticky=W)
 
+#Text
 T1 = Text(display, width=10, height=1)
 T1.insert("1.0", "07:00\n")
 T1.grid(row=15, column=1, sticky=W)
@@ -58,73 +59,86 @@ T6 = Text(display, width=10, height=1)
 T6.insert("1.0", "600\n")
 T6.grid(row=20, column=1, sticky=W)
 
+T7 = Text(display, width=28, height=10)
+T7.grid(row=23, column=0, columnspan=2, rowspan=1, sticky=W)
+ 
 #Buttons
 def b1_on_off():
-    if B1["text"] == "On":
-        B1["text"] = "Off"
-        print "timer Off"
-    if frame2["bg"] == "red":
-        frame2["bg"] = "black"
+    if B1["text"] == "Off":
+        B1["text"] = "On"
+        print "Timer On"
+        T7.insert("1.0", "Timer On\n")
+    if frame2["bg"] == "black":
+        frame2["bg"] = "red"
     else:
-         B1["text"] = "On"
-         frame2["bg"] = "red"
-         print "timer On"
-B1=Button(display, text="On", command=b1_on_off, width=4, height=1)
+         B1["text"] = "Off"
+         frame2["bg"] = "black"
+         print "timer Off"
+         T7.insert("1.0", "Timer Off\n")
+B1=Button(display, text="Off", command=b1_on_off, width=4, height=1)
 B1.grid(row=10, column=1, sticky=W)
 
 def b2_on_off():
-    if B2["text"] == "On":
-        B2["text"] = "Off"
-        print "timer Off"
-    if frame3["bg"] == "red":
-        frame3["bg"] = "black"
+    if B2["text"] == "Off":
+        B2["text"] = "On"
+        print "Moisture On"
+        T7.insert("1.0", "Moisture On\n")
+    if frame3["bg"] == "black":
+        frame3["bg"] = "red"
     else:
-         B2["text"] = "On"     
-         frame3["bg"] = "red"
-         print "timer On"
-B2=Button(display, text="On", command=b2_on_off, width=4, height=1)
+         B2["text"] = "Off"     
+         frame3["bg"] = "black"
+         print "Moisture Off"
+         T7.insert("1.0", "Moisture Off\n")
+B2=Button(display, text="Off", command=b2_on_off, width=4, height=1)
 B2.grid(row=13, column=1, sticky=W)
 
 def morning_start():
     T1_data = T1.get("1.0",END)
+    T7.insert("1.0", "%s" % T1_data)
     print T1_data
 B3=Button(display, text="Enter", command=morning_start, width=4, height=1)
 B3.grid(row=15, column=3, sticky=W)
 
 def morning_stop():
     T2_data = T2.get("1.0",END)
+    T7.insert("1.0", "%s" % T2_data)
     print T2_data
 B4=Button(display, text="Enter", command=morning_stop, width=4, height=1)
 B4.grid(row=16, column=3, sticky=W)
 
 def evening_start():
     T3_data = T3.get("1.0",END)
+    T7.insert("1.0", "%s" % T3_data)
     print T3_data
 B5=Button(display, text="Enter", command=evening_start, width=4, height=1)
 B5.grid(row=17, column=3, sticky=W)
 
 def evening_stop():
     T4_data = T4.get("1.0",END)
+    T7.insert("1.0", "%s" % T4_data)
     print T4_data
 B6=Button(display, text="Enter", command=evening_stop, width=4, height=1)
 B6.grid(row=18, column=3, sticky=W)
 
 def moisture_min():
     T5_data = T5.get("1.0",END)
+    T7.insert("1.0", "%s" % T5_data)
     print T5_data
 B7=Button(display, text="Enter", command=moisture_min, width=4, height=1)
 B7.grid(row=19, column=3, sticky=W)
 
 def moisture_max():
     T6_data = T6.get("1.0",END)
+    T7.insert("1.0", "%s" % T6_data)
     print T6_data
 B8=Button(display, text="Enter", command=moisture_max, width=4, height=1)
 B8.grid(row=20, column=3, sticky=W)
 
 #Frames
 frame1 = Frame(display, borderwidth=5, bg="black", relief="ridge", width=180, height=4)
-frame2 = Frame(display, borderwidth=3, bg="red", relief="ridge", width=50, height=25)
-frame3 = Frame(display, borderwidth=3, bg="red", relief="ridge", width=50, height=25)
+frame2 = Frame(display, borderwidth=3, bg="black", relief="ridge", width=50, height=25)
+frame3 = Frame(display, borderwidth=3, bg="black", relief="ridge", width=50, height=25)
 frame4 = Frame(display, borderwidth=5, bg="black", relief="ridge", width=200, height=4)
 frame5 = Frame(display, borderwidth=5, bg="black", relief="ridge", width=200, height=4)
 frame6 = Frame(display, borderwidth=7, relief="ridge", width=350, height=275)
@@ -135,13 +149,13 @@ frame2.grid(row=10, column=0, columnspan=2, rowspan=1, sticky=W)
 frame3.grid(row=13, column=0, columnspan=2, rowspan=1, sticky=W)
 frame4.grid(row=14, column=0, columnspan=2, rowspan=1, sticky=W)
 frame5.grid(row=21, column=0, columnspan=2, rowspan=1, sticky=W)
-frame6.grid(row=0, column=20, columnspan=2, rowspan=18, sticky=W)
-frame7.grid(row=19, column=20, columnspan=2, rowspan=18, sticky=W)
+frame6.grid(row=0, column=24, columnspan=2, rowspan=18, sticky=E)
+frame7.grid(row=19, column=24, columnspan=2, rowspan=18, sticky=W)
 #Terminal
-termf = Frame(display, height=200, width=400)
-termf.grid(row=22, column=0, columnspan=18, rowspan=1, sticky=W)
-wid = termf.winfo_id()
-os.system('xterm -into %d -geometry 60x20 -sb &' % wid)
+#termf = Frame(display, height=200, width=400)
+#termf.grid(row=22, column=0, columnspan=18, rowspan=1, sticky=W)
+#wid = termf.winfo_id()
+#os.system('xterm -into %d -geometry 60x20 -sb &' % wid)
 
 def tick():
     global time1
