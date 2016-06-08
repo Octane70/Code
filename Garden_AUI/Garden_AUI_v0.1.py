60ccaaaae0cbac4eecc13b34c817b589a63afb6d
Rob Czepil <robczepil@gmail.com>
2016-06-07 22:05:32 -0700

    Update button functions in Garden_AUI_v0.1.py

--- Garden_AUI/Garden_AUI_v0.1.py
+++ Garden_AUI/Garden_AUI_v0.1.py
@@ -33,10 +33,10 @@ class DemoPiUi(object):
         self.txt = None
         self.img = None
         self.ui = PiUi(img_dir=os.path.join(current_dir, 'imgs'))
-        self.bttn1 = "&nbsp;&nbsp;Auto&nbsp;&nbsp;"
-        self.bttn2 = "&nbsp;&nbsp;Auto&nbsp;&nbsp;"
-        self.bttn3 = "&nbsp;&nbsp;Auto&nbsp;&nbsp;"
-        self.bttn4 = "&nbsp;&nbsp;Auto&nbsp;&nbsp;"
+        self.bttn1 = "Auto"
+        self.bttn2 = "Auto"
+        self.bttn3 = "Auto"
+        self.bttn4 = "Auto"
 
     def main_menu(self):
         self.page = self.ui.new_ui_page(title="Gardenpi_UI")
@@ -58,23 +58,23 @@ class DemoPiUi(object):
         self.page.add_textbox("&nbsp;Zone1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
                               "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Manual", "h2")
         self.frame1 = self.page.add_frame_yellow("&nbsp;")
-        self.button1 = self.page.add_button("&nbsp;&nbsp;Auto&nbsp;&nbsp;", self.b1_on_off_auto)
+        self.button1 = self.page.add_button("Auto", self.b1_on_off_auto)
         self.page.add_textbox("&nbsp;")
         self.page.add_textbox("&nbsp;Zone2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
                               "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Manual", "h2")
         self.frame2 = self.page.add_frame_black("&nbsp;")
-        self.button2 = self.page.add_button("&nbsp;&nbsp;Auto&nbsp;&nbsp;", self.b2_on_off_auto)
+        self.button2 = self.page.add_button("Auto", self.b2_on_off_auto)
         self.page.add_element("hr")
         self.page.add_textbox("&nbsp;Cooling:", "h2")
         self.page.add_textbox("&nbsp;G/H Fan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
                               "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Manual", "h2")
         self.frame3 = self.page.add_frame_red("&nbsp;")
-        self.button3 = self.page.add_button("&nbsp;&nbsp;Auto&nbsp;&nbsp;", self.b3_on_off_auto)
+        self.button3 = self.page.add_button("Auto", self.b3_on_off_auto)
         self.page.add_textbox("&nbsp;")
         self.page.add_textbox("&nbsp;Case Fan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
                               "&nbsp;&nbsp;&nbsp;Manual", "h2")
         self.frame4 = self.page.add_frame_yellow("&nbsp;")
-        self.button4 = self.page.add_button("&nbsp;&nbsp;Auto&nbsp;&nbsp;", self.b4_on_off_auto)
+        self.button4 = self.page.add_button("Auto", self.b4_on_off_auto)
         self.page.add_element("hr")
         self.page.add_textbox("&nbsp;Red = On&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
                               "&nbsp;&nbsp;&nbsp;&nbsp;Yellow = Auto", "h3")
@@ -89,62 +89,62 @@ class DemoPiUi(object):
         
     #Zone 1
     def b1_on_off_auto(self):
-        if self.bttn1 == "&nbsp;&nbsp;Auto&nbsp;&nbsp;":
-            self.button1.set_text("&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;")
-            self.bttn1 = "&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;"
+        if self.bttn1 == "Auto":
+            self.button1.set_text("Off")
+            self.bttn1 = "Off"
             print "Zone1 Off"
-        elif self.bttn1 == "&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;":
-            self.button1.set_text("&nbsp;&nbsp;&nbsp;On&nbsp;&nbsp;&nbsp;&nbsp;")
-            self.bttn1 = "&nbsp;&nbsp;&nbsp;On&nbsp;&nbsp;&nbsp;&nbsp;"
+        elif self.bttn1 == "Off":
+            self.button1.set_text("On")
+            self.bttn1 = "On"
             print "Zone1 On"
         else:
-             self.button1.set_text("&nbsp;&nbsp;Auto&nbsp;&nbsp;")
-             self.bttn1 = "&nbsp;&nbsp;Auto&nbsp;&nbsp;"
+             self.button1.set_text("Auto")
+             self.bttn1 = "Auto"
              print "Zone1 Auto"
              
     #Zone 2    
     def b2_on_off_auto(self):
-        if self.bttn2 == "&nbsp;&nbsp;Auto&nbsp;&nbsp;":
-            self.button2.set_text("&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;")
-            self.bttn2 = "&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;"
+        if self.bttn2 == "Auto":
+            self.button2.set_text("Off")
+            self.bttn2 = "Off"
             print "Zone2 Off"
-        elif self.bttn2 == "&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;":
-            self.button2.set_text("&nbsp;&nbsp;&nbsp;On&nbsp;&nbsp;&nbsp;&nbsp;")
-            self.bttn2 = "&nbsp;&nbsp;&nbsp;On&nbsp;&nbsp;&nbsp;&nbsp;"
+        elif self.bttn2 == "Off":
+            self.button2.set_text("On")
+            self.bttn2 = "On"
             print "Zone2 On"
         else:
-             self.button2.set_text("&nbsp;&nbsp;Auto&nbsp;&nbsp;")
-             self.bttn2 = "&nbsp;&nbsp;Auto&nbsp;&nbsp;"
+             self.button2.set_text("Auto")
+             self.bttn2 = "Auto"
              print "Zone2 Auto"
              
     #G/H Fan   
     def b3_on_off_auto(self):
-        if self.bttn3 == "&nbsp;&nbsp;Auto&nbsp;&nbsp;":
-            self.button3.set_text("&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;")
-            self.bttn3 = "&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;"
+        if self.bttn3 == "Auto":
+            self.button3.set_text("Off")
+            self.bttn3 = "Off"
             print "G/H Fan Off"
-        elif self.bttn3 == "&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;":
-            self.button3.set_text("&nbsp;&nbsp;&nbsp;On&nbsp;&nbsp;&nbsp;&nbsp;")
-            self.bttn3 = "&nbsp;&nbsp;&nbsp;On&nbsp;&nbsp;&nbsp;&nbsp;"
+        elif self.bttn3 == "Off":
+            self.button3.set_text("On")
+            self.bttn3 = "On"
             print "G/H Fan On"
         else:
-             self.button3.set_text("&nbsp;&nbsp;Auto&nbsp;&nbsp;")
-             self.bttn3 = "&nbsp;&nbsp;Auto&nbsp;&nbsp;"
+             self.button3.set_text("Auto")
+             self.bttn3 = "Auto"
              print "G/H Fan Auto"
              
     #Case Fan
     def b4_on_off_auto(self):
-        if self.bttn4 == "&nbsp;&nbsp;Auto&nbsp;&nbsp;":
-            self.button4.set_text("&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;")
-            self.bttn4 = "&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;"
+        if self.bttn4 == "Auto":
+            self.button4.set_text("Off")
+            self.bttn4 = "Off"
             print "Case Fan Off"
-        elif self.bttn4 == "&nbsp;&nbsp;&nbsp;Off&nbsp;&nbsp;&nbsp;&nbsp;":
-            self.button4.set_text("&nbsp;&nbsp;&nbsp;On&nbsp;&nbsp;&nbsp;&nbsp;")
-            self.bttn4 = "&nbsp;&nbsp;&nbsp;On&nbsp;&nbsp;&nbsp;&nbsp;"
+        elif self.bttn4 == "Off":
+            self.button4.set_text("On")
+            self.bttn4 = "On"
             print "Case Fan On"
         else:
-             self.button4.set_text("&nbsp;&nbsp;Auto&nbsp;&nbsp;")
-             self.bttn4 = "&nbsp;&nbsp;Auto&nbsp;&nbsp;"
+             self.button4.set_text("Auto")
+             self.bttn4 = "Auto"
              print "Case Fan Auto"
 
    #def updates(self, getTempCPU):
