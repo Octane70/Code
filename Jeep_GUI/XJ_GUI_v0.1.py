ed8f3f6a27d987884027af4a5485dd09b4964f27
Rob Czepil <robczepil@gmail.com>
2017-04-05 22:39:23 -0700

    Add button press and release events

--- Jeep_GUI/XJ_GUI_v0.1.py
+++ Jeep_GUI/XJ_GUI_v0.1.py
@@ -7,11 +7,11 @@ root.maxsize(800,480)
 root.title('Jeep XJ GUI v0.1')
 
 #---Window Tabs---#
-window1 = Frame(root, borderwidth=5, relief="ridge", width=800, height=424)#Window1
-window1.grid(row=1, column=0, columnspan=800, rowspan=1, sticky=NW) #Window1 Default
-window2 = Frame(root, borderwidth=5, relief="ridge", width=800, height=424) #Window2
-window3 = Frame(root, borderwidth=5, relief="ridge", width=800, height=424) #Window3
-window4 = Frame(root, borderwidth=5, relief="ridge", width=800, height=424) #Window4
+window1 = Frame(root, borderwidth=5, relief="ridge", width=800, height=424)# Jeep Controls
+window1.grid(row=1, column=0, columnspan=800, rowspan=1, sticky=NW) # Jeep Controls (default window)
+window2 = Frame(root, borderwidth=5, relief="ridge", width=800, height=424) # Camera
+window3 = Frame(root, borderwidth=5, relief="ridge", width=800, height=424) # Jeep Diagnostics
+window4 = Frame(root, borderwidth=5, relief="ridge", width=800, height=424) # RPI Zero
 
 window1.grid_propagate(False) # Prevent window1 from resizing
 window2.grid_propagate(False) # Prevent window2 from resizing
@@ -27,7 +27,7 @@ def Window_1():
     window2.grid_remove()
     window3.grid_remove()
     window4.grid_remove()
-    window1.grid(row=1, column=0, columnspan=800, rowspan=1, sticky=NW) #Window1
+    window1.grid(row=1, column=0, columnspan=800, rowspan=1, sticky=NW) 
         
 B1=Button(root, text=" Jeep Controls", command= Window_1, width=15, height=3)
 B1.grid(row=0, column=0, sticky=NW)
@@ -36,7 +36,7 @@ def Window_2():
     window1.grid_remove()
     window3.grid_remove()
     window4.grid_remove()
-    window2.grid(row=1, column=0, columnspan=800, rowspan=1, sticky=NW) #Window2
+    window2.grid(row=1, column=0, columnspan=800, rowspan=1, sticky=NW) 
 
 B1=Button(root, text="Camera", command= Window_2, width=15, height=3)
 B1.grid(row=0, column=1, sticky=NW)
@@ -45,7 +45,7 @@ def Window_3():
     window1.grid_remove()
     window2.grid_remove()
     window4.grid_remove()
-    window3.grid(row=1, column=0, columnspan=800, rowspan=2, sticky=NW) #Window3
+    window3.grid(row=1, column=0, columnspan=800, rowspan=2, sticky=NW) 
         
 B1=Button(root, text="Jeep Diagnostics", command= Window_3, width=15, height=3)
 B1.grid(row=0, column=3, sticky=NW)
@@ -54,7 +54,7 @@ def Window_4():
     window1.grid_remove()
     window2.grid_remove()
     window3.grid_remove()
-    window4.grid(row=1, column=0, columnspan=800, rowspan=2, sticky=NW) #Window4
+    window4.grid(row=1, column=0, columnspan=800, rowspan=2, sticky=NW) 
     
 B1=Button(root, text="RPI Zero", command= Window_4, width=15, height=3)
 B1.grid(row=0, column=4, sticky=NW)
@@ -65,74 +65,139 @@ W1L1 = Label(window1, text="Door Locks:").grid(row=1, column=0, columnspan=2, st
 W1L1 = Label(window1, text="Vehicle Start:").grid(row=4, column=0, columnspan=2, sticky=W)
 
 #Window1 Frames
-W1F1 = Frame(window1, borderwidth=3, bg="yellow", relief="ridge", width=65, height=25)
-W1F1.grid(row=2, column=0, columnspan=1, rowspan=1, sticky=W)
-W1F2 = Frame(window1, borderwidth=3, bg="yellow", relief="ridge", width=65, height=25)
-W1F2.grid(row=2, column=1, columnspan=1, rowspan=1, sticky=W) 
-W1F3 = Frame(window1, borderwidth=3, bg="yellow", relief="ridge", width=65, height=25)
-W1F3.grid(row=5, column=0, columnspan=1, rowspan=1, sticky=W)
-W1F4 = Frame(window1, borderwidth=3, bg="yellow", relief="ridge", width=65, height=25)
-W1F4.grid(row=5, column=1, columnspan=1, rowspan=1, sticky=W)
+W1F1 = Frame(window1, borderwidth=3, bg="green", relief="ridge", width=65, height=25) # Door lock frame
+W1F1.grid(row=2, column=0, columnspan=1, rowspan=1, sticky=W) # Door lock grid
+W1F2 = Frame(window1, borderwidth=3, bg="green", relief="ridge", width=65, height=25) # Door unlock frame
+W1F2.grid(row=2, column=1, columnspan=1, rowspan=1, sticky=W) # Door unlock grid
+W1F3 = Frame(window1, borderwidth=3, bg="green", relief="ridge", width=65, height=25) # Vehicle start frame
+W1F3.grid(row=5, column=0, columnspan=1, rowspan=1, sticky=W) # Vehicle start grid
+W1F4 = Frame(window1, borderwidth=3, bg="green", relief="ridge", width=65, height=25) # Vehicle stop frame
+W1F4.grid(row=5, column=1, columnspan=1, rowspan=1, sticky=W) # vehicle stop grid
 
 #Window1 Buttons
-def Door_Lock():
-    print ("Doors Locked")
+#Door Lock/Unlock
+def Door_Lock_Press(event):
+    W1F1.config(bg = "red")
+    print ("Door Lock press")
     
-B1=Button(window1, text="Lock", command= Door_Lock, width=8, height=2)
+def Door_Lock_Release(event):
+    W1F1.config(bg = "green")
+    print ("Door Lock release")
+    
+B1=Button(window1, text="Lock", width=8, height=2)
 B1.grid(row=3, column=0, sticky=NW)
+B1.bind("<ButtonPress>", Door_Lock_Press)
+B1.bind("<ButtonRelease>", Door_Lock_Release)
 
-def Door_UnLock():
-    print ("Doors Unlocked")
+def Door_Unlock_Press(event):
+    W1F2.config(bg = "red")
+    print ("Door Unlock press")
+
+def Door_Unlock_Release(event):
+    W1F2.config(bg = "green")
+    print ("Door Unlock release")    
     
-B2=Button(window1, text="UnLock", command= Door_UnLock, width=8, height=2)
+B2=Button(window1, text="UnLock", width=8, height=2)
 B2.grid(row=3, column=1, sticky=NW)
+B2.bind("<ButtonPress>", Door_Unlock_Press)
+B2.bind("<ButtonRelease>", Door_Unlock_Release)
 
-def Car_Start():
-    print ("Car Start")
+#Auto Start/Stop
+def Car_Start_Press(event):
+    W1F3.config(bg = "red")
+    print ("Car Start press")
+
+def Car_Start_Release(event):
+    W1F3.config(bg = "green")
+    print ("Car Start release")    
     
-B3=Button(window1, text="Start", command= Car_Start, width=8, height=2)
+B3=Button(window1, text="Start", width=8, height=2)
 B3.grid(row=6, column=0, sticky=NW)
+B3.bind("<ButtonPress>", Car_Start_Press)
+B3.bind("<ButtonRelease>", Car_Start_Release)
 
-def Car_Stop():
-    print ("Car Stop")
+def Car_Stop_Press(event):
+    W1F4.config(bg = "red")
+    print ("Car Stop press")
+
+def Car_Stop_Release(event):
+    W1F4.config(bg = "green")
+    print ("Car Stop release")    
     
-B4=Button(window1, text="Stop", command= Car_Stop, width=8, height=2)
+B4=Button(window1, text="Stop", width=8, height=2)
 B4.grid(row=6, column=1, sticky=NW)
+B4.bind("<ButtonPress>", Car_Stop_Press)
+B4.bind("<ButtonRelease>", Car_Stop_Release)
 
 #--Window2 Layout--#
 #Window2 Frames
-W2F1 = Frame(window2, borderwidth=5, relief="ridge", width=640, height=415) # Camera Frame
-W2F1.grid(row=1, column=0, sticky=NW) # Camera Frame grid
-W2F1.grid_propagate(False) # Prevent Camera Frame from resizing
-W2F2 = Frame(window2, borderwidth=5, relief="ridge", width=150, height=415) # Window2/Frame2
-W2F2.grid(row=1, column=1, sticky=NW) # Window2/Frame2 grid
-W2F2.grid_propagate(False) # Prevent Window2/Frame2 from resizing
-W2F3 = Frame(W2F2, borderwidth=3, relief="ridge", width=65, height=25) # Capture LED
-W2F3.grid(row=1, column=2, columnspan=1, rowspan=1, sticky=W)
-W2F3.config(bg = "green")
-W2F4 = Frame(W2F2, borderwidth=3, bg="green", relief="ridge", width=65, height=25) # Record LED
-W2F4.grid(row=2, column=2, columnspan=1, rowspan=1, sticky=W)
+W2F1 = Frame(window2, borderwidth=5, relief="ridge", width=640, height=415) # Camera frame
+W2F1.grid(row=1, column=0, sticky=NW) # Camera frame grid
+W2F1.grid_propagate(False) # Prevent camera frame from resizing
+W2F2 = Frame(window2, borderwidth=5, relief="ridge", width=150, height=415) # Button frame
+W2F2.grid(row=1, column=1, sticky=NW) # Button frame grid
+W2F2.grid_propagate(False) # Prevent button frame from resizing
+W2F3 = Frame(W2F2, borderwidth=3, bg="green", relief="ridge", width=69, height=41) # Capture frame
+W2F3.grid(row=1, column=2, columnspan=1, rowspan=1, sticky=NW) # Capture frame grid
+W2F4 = Frame(W2F2, borderwidth=3, bg="green", relief="ridge", width=142, height=25) # Record frame
+W2F4.grid(row=3, column=1, columnspan=160, rowspan=1, sticky=NW) # Record frame grid
+
+#Divider Frames
+W2F5 = Frame(W2F2, borderwidth=5, bg="grey", relief="ridge", width=142, height=4) #Divider1
+W2F5.grid(row=2, column=0, columnspan=150, rowspan=1, sticky=W)  #Divider1 grid
+W2F6 = Frame(W2F2, borderwidth=5, bg="grey", relief="ridge", width=142, height=4) #Divider2
+W2F6.grid(row=5, column=0, columnspan=150, rowspan=1, sticky=W)  #Divider2 grid
+W2F7 = Frame(W2F2, borderwidth=5, bg="grey", relief="ridge", width=142, height=4) #Divider3
+W2F7.grid(row=7, column=0, columnspan=150, rowspan=1, sticky=W)  #Divider3 grid
 
 #Window2 Buttons
-def Capture_Frame():
+#Take a Picture
+def Capture_Frame_Press(event):
     W2F3.config(bg = "red")
-    print ("Capture")
-    root.after(200, lambda: W2F3.config(bg = "green" ))
-        
-B4=Button(W2F2, text="Capture", command= Capture_Frame, width=8, height=2)
-B4.grid(row=1, column=1, sticky=NW)
+    print ("Capture Press")
 
-def Record_Frame():
+def Capture_Frame_Release(event):
+    W2F3.config(bg = "green")
+    print ("Capture Release")   
+        
+W2B1=Button(W2F2, text="Capture", width=9, height=2)
+W2B1.grid(row=1, column=1, sticky=NW)
+W2B1.bind("<ButtonPress>", Capture_Frame_Press)
+W2B1.bind("<ButtonRelease>", Capture_Frame_Release)
+
+#Record Start/Stop
+def Record_Start():
+    W2F4["bg"] = "red"
     print ("Record")
     
-B4=Button(W2F2, text="Record", command= Record_Frame, width=8, height=2)
-B4.grid(row=2, column=1, sticky=NW)
+B4=Button(W2F2, text="Record", command= Record_Start, width=9, height=2)
+B4.grid(row=4, column=1, sticky=NW)
 
 def Record_Stop():
+    W2F4["bg"] = "green"
     print ("Stop Record")
     
 B4=Button(W2F2, text="Stop", command= Record_Stop, width=8, height=2)
-B4.grid(row=3, column=1, sticky=NW)
+B4.grid(row=4, column=2, sticky=NW)
+
+#Forward/Rewind
+def Skip_Back():
+    print ("Skip Back")
+    
+B4=Button(W2F2, text="Skip.R", command= Skip_Back, width=9, height=2)
+B4.grid(row=6, column=1, sticky=NW)
+
+def Skip_Forward():
+    print ("Skip Forward")
+    
+B4=Button(W2F2, text="Skip.F", command= Skip_Forward, width=8, height=2)
+B4.grid(row=6, column=2, sticky=NW)
+
+def Open_Folder():
+    print ("Open Folder")
+    
+B4=Button(W2F2, text="Open Folder", command= Open_Folder, width=19, height=1)
+B4.grid(row=8, column=1, columnspan=150, sticky=NW)
 
 root.mainloop() 
 #gpio.cleanup()	
